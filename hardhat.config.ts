58cb94aaed53b9e2bcdd1b3909469fa5698c7511
Angella-Mulikatete <mulikateteangella@gmail.com>
2024-09-11 21:14:49 +0300

    worked on private key

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,13 +3,15 @@ import "@nomicfoundation/hardhat-toolbox";
 import { vars } from "hardhat/config";
 
 const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY");
+const PRIVATE_KEY = vars.get("PRIVATE_KEY")
+
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
    networks: {
     sepolia: {
       url: "https://eth-sepolia.g.alchemy.com/v2/jQytdmthVH4e4znyeYU1M3CfWPTwbWhl", // use your Infura key
-      accounts: ["45a71309065d92d987010d97253ab26b0406f338b8de46a9c4f267d305c5d1fa"], // use your private key
+      accounts: [PRIVATE_KEY], // use your private key
     },
     
   },
